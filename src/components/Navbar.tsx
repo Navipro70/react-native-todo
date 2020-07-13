ec89a9deb2d77dc7830d49d084573d875cf0b427
Dmitry <dima.matatoff@yandex.ru>
2020-07-14 00:43:05 +0300

    Adding Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {StyleSheet, View, Text} from "react-native";
+
+export const Navbar = () => {
+    return (
+        <View style={styles.navbarView}>
+            <Text style={styles.navbarText}>Todo app</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    navbarView: {
+        flex: 0.15,
+        backgroundColor: '#ffa2e4',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        paddingBottom: 20
+    },
+    navbarText: {
+        fontSize: 24,
+        color: '#4364ff'
+    }
+})
