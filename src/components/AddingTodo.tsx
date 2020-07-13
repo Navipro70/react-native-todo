ced2be7418f7b74b6f6429ee355b044a99c7c568
Dmitry <dima.matatoff@yandex.ru>
2020-07-14 01:23:16 +0300

    Adding AddingTodo component

--- src/components/AddingTodo.tsx
+++ src/components/AddingTodo.tsx
@@ -1,10 +1,34 @@
-import React from "react";
-import {View, Text} from 'react-native'
+import React, {useState} from 'react'
+import {View, StyleSheet, Button, TextInput} from 'react-native'
 
 export const AddingTodo = () => {
+    const [inputText, setInputText] = useState<string>('')
+    const textHandler = (text: string) => {
+        setInputText(text)
+    }
     return (
-        <View>
-            <Text>Hello</Text>
+        <View style={styles.div}>
+            <TextInput style={styles.customInput} onChangeText={textHandler} value={inputText}/>
+            <Button title=" + " onPress={() => console.log(inputText)}/>
         </View>
     )
-};
+}
+
+const styles = StyleSheet.create({
+    div: {
+        flex: 0.15,
+        backgroundColor: '#91b',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+    },
+    customInput: {
+        width: '70%',
+        padding: 5,
+        color: '#fff',
+        borderColor: '#fff',
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderRadius: 10,
+    },
+})
